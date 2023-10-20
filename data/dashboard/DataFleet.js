13eb19e3a2322f8624920aa270413646360383f5
andrenogueiragbi <andrenogueiragbi@gmail.com>
2023-10-20 20:55:03 -0300

    placa de carro okay

--- data/dashboard/DataFleet.js
+++ data/dashboard/DataFleet.js
@@ -1,94 +1,95 @@
-import {
-   Briefcase,
-   ListTask,
-   People,
-   Bullseye
-} from 'react-bootstrap-icons';
+import { v4 as uuid } from 'uuid';
 
 export const Fleet = [
    {
       id: 1,
       model: "Titan 125CG",
       code: "M1",
-      icon: '/images/frota/uno.png' ,
-      marca: 'Honda'
+      icon: '/images/frota/titan.png' ,
+      marca: 'Honda',
+      placa: uuid().slice(0, 7)
    },
    {
       id: 2,
       model: "Yaris",
       code: "C2",
-      icon: '/images/frota/uno.png' ,
-      marca: 'Toyata'
+      icon: '/images/frota/yaris.png' ,
+      marca: 'Toyata',
+      placa: uuid().slice(0, 7)
    },
    {
       id: 3,
       model: "Uno",
       code: "C3",
-      icon: '/images/frota/uno.png' ,
-      marca: 'Fiat'
+      icon: '/images/frota/uno2.png' ,
+      marca: 'Fiat',
+      placa: uuid().slice(0, 7)
    },
    {
       id: 4,
       model: "HB20",
       code: "C4",
-      icon: '/images/frota/uno.png' ,
-      marca: 'Hunday'
+      icon: '/images/frota/hb20.png' ,
+      marca: 'Hunday',
+      placa: uuid().slice(0, 7)
    },
    {
-      id: 4,
+      id: 5,
       model: "HB20",
       code: "C4",
-      icon: '/images/frota/uno.png' ,
-      marca: 'Hunday'
+      icon: '/images/frota/hb20.png' ,
+      marca: 'Hunday',
+      placa: 'abc1236',
+      placa: uuid().slice(0, 7)
    },
    {
-      id: 4,
+      id: 6,
+      model: "Yaris",
+      code: "C22",
+      icon: '/images/frota/yaris.png' ,
+      marca: 'Toyata',
+      placa: uuid().slice(0, 7)
+   },
+   {
+      id: 7,
       model: "HB20",
       code: "C4",
-      icon: '/images/frota/uno.png' ,
-      marca: 'Hunday'
+      icon: '/images/frota/hb20.png' ,
+      marca: 'Hunday',
+      placa: uuid().slice(0, 7)
    },
    {
-      id: 3,
+      id: 8,
       model: "Uno",
       code: "C5",
-      icon: '/images/frota/uno.png' ,
-      marca: 'Fiat'
+      icon: '/images/frota/uno2.png' ,
+      marca: 'Fiat',
+      placa: uuid().slice(0, 7)
    },
    {
-      id: 3,
+      id: 9,
       model: "Uno",
       code: "C6",
       icon: '/images/frota/uno.png' ,
-      marca: 'Fiat'
+      marca: 'Fiat',
+      placa: uuid().slice(0, 7)
    },
+
    {
-      id: 3,
-      model: "Uno",
-      code: "C7",
-      icon: '/images/frota/uno.png' ,
-      marca: 'Fiat'
-   },
-   {
-      id: 3,
-      model: "Uno",
-      code: "C8",
-      icon: '/images/frota/uno.png' ,
-      marca: 'Fiat'
-   },
-   {
-      id: 3,
+      id: 10,
       model: "Uno",
       code: "C9",
       icon: '/images/frota/uno.png' ,
-      marca: 'Fiat'
+      marca: 'Fiat',
+      placa: uuid().slice(0, 7)
    },
    {
-      id: 3,
-      model: "Uno",
-      code: "C10",
-      icon: '/images/frota/uno.png' ,
-      marca: 'Fiat'
+      id: 11,
+      model: "HB20",
+      code: "C9",
+      icon: '/images/frota/h20-write.png' ,
+      marca: 'Fiat',
+      placa: uuid().slice(0, 7)
    },
 
 ];
--- widgets/stats/StatRightTopIconFeet.js
+++ widgets/stats/StatRightTopIconFeet.js
@@ -3,7 +3,7 @@
 
 // import node module libraries
 import PropTypes from 'prop-types';
-import { Card,Image } from 'react-bootstrap';
+import { Card, Image } from 'react-bootstrap';
 
 const StatRightTopIcon = props => {
     const { info } = props;
@@ -15,13 +15,28 @@ const StatRightTopIcon = props => {
                         <h4 className="mb-0">{info.model}</h4>
                     </div>
                     <div className="icon-shape rounded-2">
-                        <Image src={info.icon} height={50} width={100}  alt="" />
+                        <Image src={info.icon} height={50} width={90} alt="" />
                     </div>
                 </div>
-                <div>
-                    <h1 className="fw-bold">{info.code}</h1>
-                    <p className="mb-0">{info.marca}</p>
+
+                <div className="d-flex justify-content-between align-items-center mb-3">
+                    <div>
+                        <h1 className="fw-bold">{info.code}</h1>
+                        <p className="mb-0">{info.marca}</p>
+                    </div>
+
+
+                    <div style={{background: '#E8E9EA'}} className='border border-black rounded text-center'>
+                        <div style={{ fontSize: '8px' }} className="bg-primary text-center m-0 p-0">
+                            <span  className='text-white '>BRASIL</span>
+                        </div>
+                        <div className="mt-1 ms-2 me-2 fw-bold text-black">{info.placa.toUpperCase()}</div>
+                    </div>
+
+
                 </div>
+
+
             </Card.Body>
         </Card>
     )
