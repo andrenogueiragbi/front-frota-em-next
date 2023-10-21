762da2ce3762d5f8960987ba9a02c36acb3e3164
andrenogueiragbi <andrenogueiragbi@gmail.com>
2023-10-21 13:15:49 -0300

    card com place

--- widgets/stats/StatRightTopIconFeet.js
+++ widgets/stats/StatRightTopIconFeet.js
@@ -13,11 +13,11 @@ const StatRightTopIcon = props => {
 
 
     return (
-        <Card key={info.id} >
+        <Card key={info.id} className='my-custom-card' >
             <Card.Body>
                 <div className="d-flex justify-content-between align-items-center mb-3">
                     <div>
-                        <h4 className="mb-0">{info.model}</h4>
+                        <h4 className="mb-0 type_feet">{info.model}</h4>
                     </div>
                     <div className="icon-shape rounded-2">
                         <Image src={info.icon} height={50} width={90} alt="" />
@@ -26,15 +26,15 @@ const StatRightTopIcon = props => {
 
                 <div className="d-flex justify-content-between align-items-center mb-3">
                     <div>
-                        <div className="bg-primary d-flex d-flex justify-content-center align-items-center  " style={{ width: '55px', height: '55px',borderRadius: '100px' }}>
+                        <div className="ball_code d-flex d-flex justify-content-center align-items-center" style={{ width: '55px', height: '55px',borderRadius: '100px' }}>
                             <div className="d-flex justify-content-center align-items-center" >
                                 <div className="text-center">
-                                    <h1 className="text-white fs-6 badge text-wrap">C100</h1>
+                                    <h5 className="text_color_code fs-6 badge text-wrap">C100</h5>
                                 </div>
                             </div>
                         </div>
 
-                        <p className="mb-0">{info.marca}</p>
+                        <p className="mb-0 marca type_marca_feet">{info.marca}</p>
                     </div>
 
 
@@ -42,7 +42,7 @@ const StatRightTopIcon = props => {
                         <div style={{ fontSize: '8px' }} className="bg-primary d-flex justify-content-between align-items-center">
 
                             <Image className="ms-1" src={`/images/frota/mercosul.png`} height={8} width={12} alt="" />
-                            <span className='text-white'>BRASIL </span>
+                            <span className='text-white'>BRASIL</span>
                             <Image className="me-1" src={`/images/frota/brasil.png`} height={7} width={11} alt="" />
 
                         </div>
